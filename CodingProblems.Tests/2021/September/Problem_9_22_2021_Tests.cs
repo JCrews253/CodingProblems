cebc873ba1cec606723acf92e2f6e14db15d539d
James Crews <jcrews206@gmail.com>
2021-09-22 11:36:55 -0700

    git init

---
+++ CodingProblems.Tests/2021/September/Problem_9_22_2021_Tests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using CodingProblems._2021.September;
+
+namespace CodingProblems.Tests._2021.September
+{
+  [TestFixture]
+  public class Problem_9_22_2021_Tests
+  {
+    private Problem_9_22_2021 _client = new Problem_9_22_2021();
+
+    [TestCase(new[] { 10, 15, 3, 7 }, 17, true)]
+    public void ProvidedExample(int[] arr, int sum, bool expected)
+    {
+      var result = _client.ContainsSum(arr, sum);
+      Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [TestCase(new int[0], 0, false)]
+    [TestCase(new[] { 0 }, 0, false)]
+    [TestCase(new[] { 0, 0 }, 0, true)]
+    [TestCase(new[] { int.MaxValue, int.MaxValue }, 0, false)]
+    [TestCase(new[] { int.MaxValue, int.MinValue }, 0, false)]
+    [TestCase(new[] { int.MinValue, int.MaxValue }, 0, false)]
+    [TestCase(new[] { int.MinValue, int.MinValue }, 0, false)]
+    [TestCase(new[] { int.MaxValue, int.MinValue + 1 }, 0, true)]
+    [TestCase(new[] { int.MinValue + 1, int.MaxValue }, 0, true)]
+    [TestCase(new[] { -8, -7, -4, -1,  9 },  8, true)]
+    [TestCase(new[] { -6, -3, -2,  7,  9 },  5, true)]
+    [TestCase(new[] { -5, -1,  0,  4,  6 }, 10, true)]
+    [TestCase(new[] { -9, -8, -4, -2,  8 }, -6, true)]
+    [TestCase(new[] { -9, -8, -3, -2,  2 },  0, true)]
+    [TestCase(new[] { -3,  3,  5,  8,  9 },  1, false)]
+    [TestCase(new[] { -9, -8, -4, -1,  8 }, -3, false)]
+    [TestCase(new[] { -3, -2,  3,  7,  9 },  3, false)]
+    [TestCase(new[] { -2, -1,  1,  8, 10 },  5, false)]
+    [TestCase(new[] { -9, -2,  1,  2,  4 },  7, false)]
+    public void CreatedTest(int[] arr, int sum, bool expected)
+    {
+      var result = _client.ContainsSum(arr, sum);
+      Assert.That(result, Is.EqualTo(expected));
+    }
+  }
+}
---
+++ CodingProblems/2021/September/Problem_9_22_2021.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace CodingProblems._2021.September
+{
+  public class Problem_9_22_2021
+  {
+    // Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+    // For example, given[10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+    public bool ContainsSum(int[] arr, int sum)
+    {
+      if (arr.Length < 2)
+      {
+        return false;
+      }
+      var hs = new HashSet<int>();
+
+      for (int i = 0; i < arr.Length; i++)
+      {
+        try
+        {
+          int complement = checked(sum - arr[i]);
+          if (hs.Contains(complement))
+          {
+            return true;
+          }
+          else
+          {
+            hs.Add(arr[i]);
+          }
+        }
+        catch
+        {
+          hs.Add(arr[i]);
+        }
+      }
+      return false;
+    }
+  }
+}

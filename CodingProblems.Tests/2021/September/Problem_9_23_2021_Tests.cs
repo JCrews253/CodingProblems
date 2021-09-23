f4ac7a2a4cb359c60d3744f669124ee22fa5ff64
James Crews <jcrews206@gmail.com>
2021-09-23 09:26:39 -0700

    9/23/2021

---
+++ CodingProblems.Tests/2021/September/Problem_9_23_2021_Tests.cs
@@ -0,0 +1,34 @@
+﻿using CodingProblems._2021.September;
+using NUnit.Framework;
+
+namespace CodingProblems.Tests._2021.September
+{
+  [TestFixture]
+  public class Problem_9_23_2021_Tests
+  {
+    private Problem_9_23_2021 _client = new Problem_9_23_2021();
+
+    [TestCase(new[] { 1, 2, 3, 4, 5 }, new[] { 120, 60, 40, 30, 24 })]
+    [TestCase(new[] { 3, 2, 1 }, new[] { 2, 3, 6 })]
+    public void ProvidedExample(int[] arr, int[] expected)
+    {
+      var result = _client.WithDivision(arr);
+      var result2 = _client.WithoutDivision(arr);
+      Assert.That(result, Is.EqualTo(expected));
+      Assert.That(result2, Is.EqualTo(expected));
+    }
+
+    // Assume no overflows occur and no zeros
+    [TestCase(new[] { -8, -7, -4, -1,  9 }, new[] { -252, -288, -504,  -2016,  224 })]
+    [TestCase(new[] { -6, -3, -2,  7,  9 }, new[] {  378,  756, 1134,   -324, -252 })] 
+    [TestCase(new[] { -9, -8, -4, -2,  8 }, new[] { -512, -576, -1152, -2304,  576 })]
+    [TestCase(new[] { -9, -8, -3, -2,  2 }, new[] {  -96, -108, -288,   -432,  432 })]
+    public void CreatedTest(int[] arr, int[] expected)
+    {
+      var result = _client.WithDivision(arr);
+      var result2 = _client.WithoutDivision(arr);
+      Assert.That(result, Is.EqualTo(expected));
+      Assert.That(result2, Is.EqualTo(expected));
+    }
+  }
+}
---
+++ CodingProblems/2021/September/Problem_9_23_2021.cs
@@ -0,0 +1,75 @@
+﻿namespace CodingProblems._2021.September
+{
+  //Given an array of integers, return a new array such that each element
+  //at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+  // For example, if our input was[1, 2, 3, 4, 5], the expected output would be[120, 60, 40, 30, 24].
+  // If our input was[3, 2, 1], the expected output would be[2, 3, 6].
+  public class Problem_9_23_2021
+  {
+    public int[] WithDivision(int[] arr)
+    {
+      var totalProduct = 1;
+      for (int i = 0; i < arr.Length; i++)
+      {
+        totalProduct = totalProduct * arr[i];
+      }
+
+      var newArr = new int[arr.Length];
+      for (int i = 0; i < arr.Length; i++)
+      {
+        newArr[i] = totalProduct / arr[i];
+      }
+      return newArr;
+    }
+
+    // Follow-up: what if you can't use division?
+    public int[] WithoutDivision(int[] arr)
+    {
+      var forward = new int[arr.Length];
+      var backward = new int[arr.Length];
+
+      for(int i = 0; i < arr.Length; i++)
+      {
+        if(i == 0)
+        {
+          forward[i] =  arr[i];
+        }
+        else
+        {
+          forward[i] = forward[i - 1] * arr[i];
+        }  
+      }
+
+      for(int i = arr.Length - 1; i > -1; i--)
+      {
+        if(i == arr.Length - 1)
+        {
+          backward[i] = arr[i];
+        }
+        else
+        {
+          backward[i] = backward[i + 1] * arr[i];
+        }
+      }
+
+      var newArr = new int[arr.Length];
+      for(int i = 0; i < arr.Length; i++)
+      {
+        if(i == 0)
+        {
+          newArr[i] = backward[1];
+        }
+        else if(i == arr.Length - 1)
+        {
+          newArr[i] = forward[i - 1];
+        }
+        else
+        {
+          newArr[i] = forward[i - 1] * backward[i + 1];
+        }
+      }
+      return newArr;
+    }
+  }
+}

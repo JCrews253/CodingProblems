38b04fa2c97d4cf15928bf7fed457ae47c27ee46
James Crews <jcrews206@gmail.com>
2021-09-24 10:58:32 -0700

    9/24/2021

---
+++ CodingProblems.Tests/2021/September/Problem_9_24_2021_Tests.cs
@@ -0,0 +1,63 @@
+﻿using CodingProblems._2021.September;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems.Tests._2021.September
+{
+  [TestFixture]
+  class Problem_9_24_2021_Tests
+  {
+    private Problem_9_24_2021 _client = new Problem_9_24_2021();
+
+
+    //The following test should pass:
+    // node = Node('root', Node('left', Node('left.left')), Node('right'))
+    // assert deserialize(serialize(node)).left.left.val == 'left.left'
+    [Test]
+    public void ProvidedExample()
+    {
+      var node = new Node("root", new Node("left", new Node("left.left", null, null), null), null);
+      var result = _client.Deserialize(_client.Serialize(node)).Left.Left.Val;
+      Assert.That(result, Is.EqualTo("left.left"));
+    }
+
+    [Test]
+    public void CreatedTest()
+    {
+      //               root 
+      //               /  \
+      //            left   right
+      //           /    \
+      //          /      \
+      //         /        \
+      //    left.left  left.right
+      var node = new Node("root", new Node("left", new Node("left.left", null, null), new Node("left.right", null, null)), new Node("right", null, null));
+      var deserialized = _client.Deserialize(_client.Serialize(node));
+      var root = deserialized.Val;
+      var rootLeft = deserialized.Left.Val;
+      var rootRight = deserialized.Right.Val;
+      var rootLeftLeft = deserialized.Left.Left.Val;
+      var rootLeftRight = deserialized.Left.Right.Val;
+
+      var rootLeftLeftLeft = deserialized.Left.Left.Left;
+      var rootLeftLeftRight = deserialized.Left.Left.Right;
+      var rootRightLeft = deserialized.Right.Left;
+      var rootRightRight = deserialized.Right.Right;
+
+      Assert.That(root, Is.EqualTo("root"));
+      Assert.That(rootLeft, Is.EqualTo("left"));
+      Assert.That(rootRight, Is.EqualTo("right"));
+      Assert.That(rootLeftLeft, Is.EqualTo("left.left"));
+      Assert.That(rootLeftRight, Is.EqualTo("left.right"));
+
+      Assert.That(rootLeftLeftLeft, Is.Null);
+      Assert.That(rootLeftLeftRight, Is.Null);
+      Assert.That(rootRightLeft, Is.Null);
+      Assert.That(rootRightRight, Is.Null);
+    }
+  }
+}
---
+++ CodingProblems/2021/September/Problem_9_24_2021.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems._2021.September
+{
+  // Given the root to a binary tree, implement serialize(root),
+  // which serializes the tree into a string, and deserialize(s),
+  // which deserializes the string back into the tree.
+
+  // class Node:
+  // def __init__(self, val, left= None, right= None):
+  //      self.val = val
+  //      self.left = left
+  //      self.right = right
+
+  public record Node(string Val, Node Left, Node Right);
+
+  public class Problem_9_24_2021
+  {
+    private int _index;
+
+    public string Serialize(Node root)
+    {
+      if(root == null)
+      {
+        return "null";
+      }
+      return root.Val + ',' + Serialize(root.Left) + ',' + Serialize(root.Right);
+    }
+
+    public Node Deserialize(string data)
+    {
+      _index = 0;
+      return DeserializeHelper(data.Split(','));
+    }
+
+    private Node DeserializeHelper(string[] data)
+    { 
+      if(_index >= data.Length)
+      {
+        return null;
+      }
+      var  current = data[_index++];
+      if(current == "null")
+      {
+        return null;
+      }
+      var node = new Node(current, DeserializeHelper(data), DeserializeHelper(data));
+      return node;
+    }
+  }
+}
